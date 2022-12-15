736f7e641a773c67c775d0bde29b64c36a1ea7f5
MartinelliFabio <fabio.martinelli996@gmail.com>
2022-12-15 17:54:36 +0100

    Added _variable.scss

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,18 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container container-comics">
-        <div class="comics-main">
-            <div class="row d-flex flex-wrap">
-                @foreach ($comics as $comic)
-                <div class="my-card">
-                    <div class="img-box">
-                        <img src="{{$comic['thumb']}}" alt="">
+    <div class="main">
+        <div class="container container-comics">
+            <div class="comics-main">
+                <div class="row d-flex flex-wrap">
+                    @foreach ($comics as $comic)
+                    <div class="my-card">
+                        <div class="img-box">
+                            <img src="{{$comic['thumb']}}" alt="">
+                        </div>
+                        <p class="text-white">{{$comic['series']}}</p>
                     </div>
-                    <p class="text-white">{{$comic['series']}}</p>
+                    @endforeach
                 </div>
-                @endforeach
             </div>
+        </div>
+        <div class="btn-box">
+            <a href="#nogo"><button>Load more</button></a>
         </div>
     </div>
 @endsection
